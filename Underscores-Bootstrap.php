d6bef5059f68f94c4c6247918be92f41715b203e
Daniel Warner <contact@dfos.ca>
2014-04-25 14:20:30 -0600

    Changed Underscores stylesheet to minified version.

--- Underscores-Bootstrap.php
+++ Underscores-Bootstrap.php
@@ -1,5 +1,5 @@
 function theme_scripts() {
-	wp_register_style( 'underscores', get_template_directory_uri() . '/css/underscores.css', array(), '3.0.1', 'all' );
+	wp_register_style( 'underscores', get_template_directory_uri() . '/css/underscores.min.css', array(), '3.0.1', 'all' );
 
 	wp_enqueue_style( 'underscores' );
 
